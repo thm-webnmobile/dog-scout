3c610012df6172c6a6f5be5f9bc5fc17dd855998
Yen Nhi Ho <yen.nhi.ho@iem.thm.de>
2019-11-22 21:57:07 +0100

    integration of leaflet map

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,11 +1,28 @@
 import React, { Component } from "react";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
 import { Container, Row, Col, Image } from "react-bootstrap";
 import "./Map.css";
 
 class Map extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    const mymap = L.map("mapid").setView([50.337175, 8.736563], 10);
+    const attribution =
+      '&copy; <a href = "https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+    const tileUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+    const tiles = L.tileLayer(tileUrl, { attribution });
+    tiles.addTo(mymap);
+  }
+
   render() {
     return (
-      <Image className="map" src="pics/map_placeholder.PNG" height="610" />
+      <div id="mapid"></div>
+      // <Image className="map" src="pics/map_placeholder.PNG" height="610" />
     );
   }
 }
